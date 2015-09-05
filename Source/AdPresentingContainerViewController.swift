4e8c881134a2bf714b673866768926770f86467c
Jaanus Siim <jaanus@jaanussiim.com>
2015-09-05 10:06:12 +0300

    Separate iAd from ad presenting controller

--- Source/AdPresentingContainerViewController.swift
+++ Source/AdPresentingContainerViewController.swift
@@ -15,42 +15,46 @@
 */
 
 import UIKit
-import iAd
 
-public class AdPresentingContainerViewController: UIViewController, ADBannerViewDelegate {
+public class AdPresentingContainerViewController: UIViewController {
+    @IBOutlet var contentPresentingView: UIView?
     @IBOutlet var adContainerView: UIView!
     @IBOutlet var adContainerHeightConstraint: NSLayoutConstraint!
+    public var presented: UIViewController?
     
-    var banner: ADBannerView?
+    var banner: UIView!
     
     public override func viewDidLoad() {
         super.viewDidLoad()
         
         adContainerHeightConstraint.constant = 0
-    }
-    
-    public override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    public override func viewDidAppear(animated: Bool) {
-        loadAd()
-    }
-    
-    func loadAd() {
-        if let banner = self.banner {
+        
+        guard let show = presented else {
             return
         }
         
-        let adBanner = ADBannerView(adType: ADAdType.Banner)
-        self.banner = adBanner
-        adBanner.delegate = self
-        adBanner.autoresizingMask = [UIViewAutoresizing.FlexibleLeftMargin, UIViewAutoresizing.FlexibleTopMargin, UIViewAutoresizing.FlexibleRightMargin, UIViewAutoresizing.FlexibleBottomMargin]
-        adContainerView.addSubview(adBanner)
+        guard let container = contentPresentingView else {
+            return
+        }
+        
+        addChildViewController(show)
+
+        show.view.frame = container.bounds
+        show.view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
+        container.addSubview(show.view)
     }
     
-    public func bannerViewDidLoadAd(banner: ADBannerView!) {
+    public override func viewDidAppear(animated: Bool) {
+        if let _ = banner {
+            return
+        }
+        
+        banner = createBanner()
+        banner.autoresizingMask = [UIViewAutoresizing.FlexibleLeftMargin, UIViewAutoresizing.FlexibleTopMargin, UIViewAutoresizing.FlexibleRightMargin, UIViewAutoresizing.FlexibleBottomMargin]
+        adContainerView.addSubview(banner)
+    }
+    
+    public func showBanner(banner: UIView) {
         banner.center = CGPointMake(CGRectGetWidth(adContainerView.frame) / 2, CGRectGetHeight(adContainerView.frame) / 2)
         UIView.animateWithDuration(0.3, animations: { () -> Void in
             self.adContainerHeightConstraint.constant = CGRectGetHeight(banner.frame)
@@ -58,10 +62,14 @@ public class AdPresentingContainerViewController: UIViewController, ADBannerView
         })
     }
     
-    public func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+    public func hideBanner(banner: UIView) {
         UIView.animateWithDuration(0.3, animations: { () -> Void in
             self.adContainerHeightConstraint.constant = 0
             self.view.layoutIfNeeded()
         })
+    }
+    
+    public func createBanner() -> UIView {
+        fatalError("Override createBanner")
     }
 }
