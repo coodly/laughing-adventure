73309ded598b0fc0290b68983cfaf4fdaf86b829
Jaanus Siim <jaanus@jaanussiim.com>
2017-01-27 17:58:43 +0200

    Idea for fetched collection view

---
+++ Source/UI/FetchedCollectionView.swift
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2017 Coodly LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import Foundation
+import UIKit
+import CoreData
+
+open class FetchedCollectionView<Model: NSManagedObject, Cell: UICollectionViewCell>: UICollectionView, UICollectionViewDataSource, NSFetchedResultsControllerDelegate {
+    public var fetchedController: NSFetchedResultsController<Model>? {
+        didSet {
+            registerCell(forType: Cell.self)
+            dataSource = self
+            fetchedController?.delegate = self
+        }
+    }
+    private var changeActions = [ChangeAction]()
+    
+    public func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return fetchedController?.sections?.count ?? 0
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let controller = fetchedController else {
+            return 0
+        }
+        
+        let sections:[NSFetchedResultsSectionInfo] = controller.sections! as [NSFetchedResultsSectionInfo]
+        return sections[section].numberOfObjects
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(for: indexPath) as Cell
+        let object = fetchedController!.object(at: indexPath)
+        return cell
+    }
+    
+    public func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        Logging.log("controllerWillChangeContent")
+        changeActions.removeAll()
+    }
+    
+    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange sectionInfo: NSFetchedResultsSectionInfo, atSectionIndex sectionIndex: Int, for type: NSFetchedResultsChangeType) {
+        changeActions.append(ChangeAction(sectionIndex: sectionIndex, indexPath: nil, newIndexPath: nil, changeType: type))
+    }
+    
+    public func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        changeActions.append(ChangeAction(sectionIndex: nil, indexPath: indexPath, newIndexPath: newIndexPath, changeType: type))
+    }
+    
+    public func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        Logging.log("controllerDidChangeContent: \(changeActions.count) changes")
+        let visible = indexPathsForVisibleItems
+        
+        let updateClosure = {
+            // update sections
+            let sectionActions = self.changeActions.filter({ $0.sectionIndex != nil })
+            Logging.log("\(sectionActions.count) section actions")
+            
+            let sectionInserts = sectionActions.filter({ $0.changeType == .insert }).map({ $0.sectionIndex! })
+            self.insertSections(IndexSet(sectionInserts))
+            
+            let sectionDeletes = sectionActions.filter({ $0.changeType == .insert }).map({ $0.sectionIndex! })
+            self.deleteSections(IndexSet(sectionDeletes))
+            
+            assert(sectionActions.filter({ $0.changeType != .insert && $0.changeType != .delete}).count == 0)
+            
+            let cellActions = self.changeActions.filter({ $0.sectionIndex == nil })
+            Logging.log("\(cellActions.count) cell actions")
+            
+            var cellUpdates = cellActions.filter({ $0.changeType == .update })
+            self.reloadItems(at: cellUpdates.map({ $0.indexPath! }))
+            
+            let cellInserts = cellActions.filter({ $0.changeType == .insert }).map({ $0.newIndexPath! })
+            self.insertItems(at: cellInserts)
+            
+            let cellDeletes = cellActions.filter({ $0.changeType == .delete}).map({ $0.indexPath! })
+            self.deleteItems(at: cellDeletes)
+            
+            let moveActions = cellActions.filter({ $0.changeType == .move})
+            for action in moveActions {
+                self.moveItem(at: action.indexPath!, to: action.newIndexPath!)
+            }
+        }
+        
+        let completion: (Bool) -> () = {
+            finished in
+            
+            self.contentChanged()
+        }
+        
+        performBatchUpdates(updateClosure, completion: completion)
+    }
+    
+    open func contentChanged() {
+        Logging.log("\(#function)")
+    }
+
+}
