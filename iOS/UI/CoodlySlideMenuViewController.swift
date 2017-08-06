eca86b13359bf4c0becb78d1b87efde29b0a0992
Jaanus Siim <jaanus@jaanussiim.com>
2017-08-06 09:43:19 +0300

    Override

--- iOS/UI/CoodlySlideMenuViewController.swift
+++ iOS/UI/CoodlySlideMenuViewController.swift
@@ -36,7 +36,7 @@ open class CoodlySlideMenuViewController: SlideMenuController {
     }
 
     public required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     override open func viewWillAppear(_ animated: Bool) {
